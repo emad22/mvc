e64468f262a5df5402662a4d358fe34f9344aeff
Emad Rashad <emadrashad22@gmail.com>
2024-05-02 12:19:57 +0300

    Crerate URL file

---
+++ vendor/System/Url.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace System;
+
+class Url
+{
+    protected $app;
+
+    public function __construct(Application $app){
+        $this->app = $app;
+    }
+    public function link($path){
+        return $this->app->request->baseUrl() . trim($path, '/');
+    }
+    public function redirectTo($path){
+        header('location:' . $this->link($path));
+        exit;
+    } 
+
+}
